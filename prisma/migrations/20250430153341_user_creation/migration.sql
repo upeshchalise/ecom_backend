fac8ad01da68cf85044505b0eb9d6bf4f21b8b78
upeshchalise <upeshchalise@gmail.com>
2025-04-30 23:57:35 +0545

    backend setup --progress

---
+++ prisma/migrations/20250430153341_user_creation/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('BUYER', 'SELLER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT,
+    "phone" TEXT,
+    "address" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'BUYER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- src/apps/ecom/backend/start.ts
+++ src/apps/ecom/backend/start.ts
@@ -1,18 +1,18 @@
-// import { Configuration } from '../../../../config/index';
-import { Container } from './Container';
-import { Server } from './Server';
+// // import { Configuration } from '../../../../config/index';
+// import { Container } from './Container';
+// import { Server } from './Server';
 
-const container = new Container();
-// const server = container.invoke().resolve<Server>('server');
-// const config = container.invoke().resolve<Configuration>('config');
-// const seeder = container.invoke().resolve<CreateSuperAdminSeeder>('createSuperAdminSeeder');
+// const container = new Container();
+// // const server = container.invoke().resolve<Server>('server');
+// // const config = container.invoke().resolve<Configuration>('config');
+// // const seeder = container.invoke().resolve<CreateSuperAdminSeeder>('createSuperAdminSeeder');
 
-server
-  .start()
-  .then(async () => {
-   console.log('Server started successfully');
-     })
-  .catch((err: Error) => {
-    console.log(err);
-    process.exit(1);
-  });
+// server
+//   .start()
+//   .then(async () => {
+//    console.log('Server started successfully');
+//      })
+//   .catch((err: Error) => {
+//     console.log(err);
+//     process.exit(1);
+//   });
